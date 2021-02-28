63b5869c7f6d09a2e97bc3b729006b08b69e519b
jtorres01 <jonathan.torres19@stjohns.edu>
2021-02-27 19:34:48 -0500

    First Test

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -10,6 +10,7 @@ public class UniqueWords
    public static int countUnique(ArrayList<String> list)
    {
 	  int count = 0;
+	  //
 	  
       for (int i = 0; i < list.size(); i++)
       {		 for (int j = 0; j < list.size(); j++)
